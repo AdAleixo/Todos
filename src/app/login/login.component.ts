6e8ea3a7cecc9a9a33f043ca43471952559fd075
adaleixo <isaac.aleixo@gmail.com>
2021-02-09 18:29:43 -0300

    Rotas criadas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  username = "IsaacAleixo";
+  password = "";
+  errorMessage = "Invalid credentials";
+  invalidLogin = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleLogin() {
+    if (this.username === "IsaacAleixo" && this.password === "dummy") {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
   }
-
 }
